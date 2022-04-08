20acbc038acefc78a70a1ce9ea8eb436fe46027b
Maciej Grochowski <mghej3@wp.pl>
2022-04-09 00:01:40 +0200

    Initial commit

---
+++ src/components/StartingPage/StartingPageContent.js
@@ -0,0 +1,11 @@
+import classes from './StartingPageContent.module.css';
+
+const StartingPageContent = () => {
+  return (
+    <section className={classes.starting}>
+      <h1>Witaj w moim projekcie!</h1>
+    </section>
+  );
+};
+
+export default StartingPageContent;
